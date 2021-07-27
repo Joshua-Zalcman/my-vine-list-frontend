40c1f25bd7313f58d1e7b18af45388f6d579af92
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-27 11:33:36 -0400

    adds api call for wine list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+	const [wineList, setWineList] = useState([]);
+	URL = 'http://localhost:8000/api/wines/';
+	useEffect(() => {
+		getWines();
+	}, []);
+
+	const getWines = async () => {
+		const wines = await axios.get(URL);
+		console.log(wines.data);
+	};
+
 	return (
 		<div className="App">
 			<h1>My Vine List</h1>
