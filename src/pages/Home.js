0a7206b6ec60559874859d668b0a861f76338607
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-08-02 15:17:15 -0400

    updates home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Container, Jumbotron } from 'reactstrap';
 
 const Home = () => {
 	return (
 		<Container className="text-center">
-			<h1>Welcome to My Vine List</h1>
-			<p>Record your wine purchases and keep track of what you like to sip.</p>
+			<Jumbotron fluid style={{ backgroundColor: 'transparent' }}>
+				<Container fluid>
+					{' '}
+					<h1 className="display-4">Welcome to My Vine List</h1>
+					<p>
+						Record your wine purchases and keep track of what you like to sip.
+					</p>
+					<p className="lead">
+						<Link className="btn btn-primary btn-lg" to="/login" role="button">
+							Login
+						</Link>
+					</p>
+					<p className="lead">
+						New User? Register <Link to="/register">here.</Link>
+					</p>
+				</Container>
+			</Jumbotron>
 		</Container>
 	);
 };
